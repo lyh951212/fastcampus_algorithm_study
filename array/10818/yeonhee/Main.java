7edd4c3a6787466f426bf59f22d254a20bc0dbd2
YeonHee <lyh951212@naver.com>
2022-11-01 17:26:02 +0900

    이연희 10818 코드 커밋

---
+++ array/10818/yeonhee/Main.java
@@ -0,0 +1,26 @@
+package yeonhee;
+
+import java.util.*;
+import java.io.BufferedReader;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+
+        // 배열 생성
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for(int i = 0; i < n ; ++i)
+        {
+            arr.add(sc.nextInt());
+        }
+
+        // 정렬
+        Collections.sort(arr);
+
+        // 출력
+        System.out.print(arr.get(0) + " " + arr.get(n-1));
+    }
+}
