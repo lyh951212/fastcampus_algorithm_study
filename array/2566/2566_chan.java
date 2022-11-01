11f390b5e1c693bf8dd9aa3a6dcd7acfbc33c436
Chan <seulchan07@gmail.com>
2022-11-01 15:10:07 +0900

    update solution
    백준 2566

---
+++ array/2566/2566_chan.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int max = 0;
+        int n = 9;
+        int row = 1;
+        int col = 1;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < n; j++) {
+                int number = Integer.parseInt(st.nextToken());
+                if (number > max) {
+                    max = number;
+                    row = i + 1;
+                    col = j + 1;
+                }
+            }
+        }
+
+        bw.write(String.valueOf(max));
+        bw.newLine();
+        bw.write(row + " " + col);
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
