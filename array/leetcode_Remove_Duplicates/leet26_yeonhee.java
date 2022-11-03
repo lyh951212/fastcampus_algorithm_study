2a4104fbe457c0b596ad8db865eee0bc1089641d
YeonHee <lyh951212@naver.com>
2022-11-03 21:24:44 +0900

    Create leet26_yeonhee.java

---
+++ array/leetcode_Remove_Duplicates/leet26_yeonhee.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int curValue = nums[0];
+        int curIdx = 1;
+
+        for(int i = 1; i < nums.length; ++i)
+        {
+            if(curValue != nums[i])
+            {
+                nums[curIdx++]  = nums[i];
+                curValue        = nums[i];
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        return curIdx;
+    }
+}
+// 	13 ms,	48 MB
