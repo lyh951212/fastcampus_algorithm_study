c38a34d6fc46082ce2b12f0eeeaa5eb1a0cd9081
Chan <seulchan07@gmail.com>
2022-11-09 12:17:12 +0900

    Update 2164-chan.java

--- queue and stack/2164/2164-chan.java
+++ queue and stack/2164/2164-chan.java
@@ -6,14 +6,17 @@ import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 
+// 백준에서는 큰 값을 N으로 넣어주지 않아서 시간 차이는 크게 나지 않았음.
+// 내 맥북에서 N 값을 10000000로 주고 돌려보니 LinkedList는 2452ms ArrayDeque는 284ms 
+// 거의 8-9배의 성능차이를 보여줌
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-//        Deque<Integer> cards = new ArrayDeque<>(); // 236 ms
-        Queue<Integer> cards = new LinkedList<>(); // 188 ms
-
+//        Queue<Integer> cards = new ArrayDeque<>(); // 29212KB 204 ms
+        Queue<Integer> cards = new LinkedList<>(); // 45708KB 196 ms
         int N = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < N; i++) {
