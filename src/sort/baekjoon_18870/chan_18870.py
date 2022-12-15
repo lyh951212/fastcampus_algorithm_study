e12fc3e1eef5e36990785cae0e51c80fb7437bd0
Chan <seulchan07@gmail.com>
2022-12-15 17:26:54 +0900

    update 백준 18870

---
+++ src/sort/baekjoon_18870/chan_18870.py
@@ -0,0 +1,13 @@
+import sys
+
+# 149744 KB // 1844 ms
+if __name__ == "__main__":
+    num = int(sys.stdin.readline())
+    x_axis_list = list(map(int, sys.stdin.readline().split()))
+
+    index_map = {}
+    for index, value in enumerate(sorted(set(x_axis_list))):
+        index_map[value] = index
+
+    for x in x_axis_list:
+        print(index_map.get(x), end=" ")
