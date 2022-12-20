7dba28a7094465ca005f65840eca781306c9f4a4
lyh951212 <lyh951212@naver.com>
2022-12-21 00:14:39 +0900

    재귀 과제 1 백준15657번 커밋

--- src/recursion/baekjoon_15657/yeonhee_15657.py
+++ src/recursion/baekjoon_15657/yeonhee_15657.py
@@ -1,2 +1,18 @@
+import sys
+# 	30616kb,	56ms
+def recursion(nums, m, tmp_list, cur_idx):
+    if len(tmp_list) == m :
+        print(*tmp_list)
+        return
 
-print(1)
+    for idx in range(cur_idx, len(nums)):
+        tmp_list.append(nums[idx])
+        recursion(nums, m, tmp_list, idx)
+        tmp_list.pop(len(tmp_list)-1)
+
+
+n, m = map(int, sys.stdin.readline().rstrip().split())
+nums = list(map(int, sys.stdin.readline().split()))
+nums.sort()
+recursion(nums, m, list(), 0)
+
